f90a00aaa4816c72c1c9b069b61b9e863cebdc9b
David Stygstra <david.stygstra@gmail.com>
2014-10-15 03:42:00 -0400

    More tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -93,3 +93,58 @@ fn gamma(seed: u32, shape: f64, scale: f64) -> bool {
 fn geometric(seed: u32, p: f64) -> bool {
     np(seed, "geometric", (p,)) == rk(seed, dist::Geometric::new(p).ok())
 }
+
+#[quickcheck]
+fn gumbel(seed: u32, loc: f64, scale: f64) -> bool {
+    np(seed, "gumbel", (loc, scale)) == rk(seed, dist::Gumbel::new(loc, scale).ok())
+}
+
+// TODO: `Testable` is not implemented for `fn(u32, int, int, int) -> bool`
+//#[quickcheck]
+//fn hypergeometric(seed: u32, ngood: int, nbad: int, nsample: int) -> bool {
+//    np(seed, "hypergeometric", (ngood, nbad, nsample)) == rk(seed, dist::Hypergeometric::new(ngood, nbad, nsample).ok())
+//}
+
+#[quickcheck]
+fn laplace(seed: u32, loc: f64, scale: f64) -> bool {
+    np(seed, "laplace", (loc, scale)) == rk(seed, dist::Laplace::new(loc, scale).ok())
+}
+
+#[quickcheck]
+fn logistic(seed: u32, loc: f64, scale: f64) -> bool {
+    np(seed, "logistic", (loc, scale)) == rk(seed, dist::Logistic::new(loc, scale).ok())
+}
+
+#[quickcheck]
+fn lognormal(seed: u32, mean: f64, sigma: f64) -> bool {
+    np(seed, "lognormal", (mean, sigma)) == rk(seed, dist::Lognormal::new(mean, sigma).ok())
+}
+
+#[quickcheck]
+fn logseries(seed: u32, p: f64) -> bool {
+    np(seed, "logseries", (p,)) == rk(seed, dist::Logseries::new(p).ok())
+}
+
+// TODO: convert Vec<f64> to numpy array
+//#[quickcheck]
+//fn multinomial(seed: u32, pvals: Vec<f64>) -> bool {
+//    np(seed, "multinomial", (pvals,)) == rk(seed, dist::Multinomial::new(pvals).ok())
+//}
+
+#[quickcheck]
+fn negative_binomial(seed: u32, n: f64, p: f64) -> bool {
+    np(seed, "negative_binomial", (n, p)) == rk(seed, dist::NegativeBinomial::new(n, p).ok())
+}
+
+#[quickcheck]
+fn noncentral_chisquare(seed: u32, df: f64, nonc: f64) -> bool {
+    np(seed, "noncentral_chisquare", (df, nonc)) == rk(seed, dist::NoncentralChisquare::new(df, nonc).ok())
+}
+
+// TODO: `Testable` is not implemented for `fn(u32, f64, f64, f64) -> bool`
+//#[quickcheck]
+//fn noncentral_f(seed: u32, dfnum: f64, dfden: f64, nonc: f64) -> bool {
+//    np(seed, "noncentral_f", (dfnum, dfden, nonc)) == rk(seed, dist::NoncentralF::new(dfnum, dfden, nonc).ok())
+//}
+
+// TODO: finish writing tests
